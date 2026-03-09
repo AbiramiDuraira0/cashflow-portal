7ed87e28ca230c0758c610c2ef00a6bd3a851ff5
Abirami Durairaj <adurai310@apac.comcast.com>
2026-03-09 20:54:34 +0530

    Added images for browser tab logo , vertical side menu, hover image change for side menu , colpase and expand side menu

--- cashflow-portal/src/app/app.ts
+++ cashflow-portal/src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'cashflow-portal';
+  protected isMenuCollapsed = true;
+  protected isMenuHovered = false;
+
+  toggleMenu() {
+    this.isMenuCollapsed = !this.isMenuCollapsed;
+  }
+
+  onMenuMouseEnter() {
+    this.isMenuHovered = true;
+  }
+
+  onMenuMouseLeave() {
+    this.isMenuHovered = false;
+  }
 }
