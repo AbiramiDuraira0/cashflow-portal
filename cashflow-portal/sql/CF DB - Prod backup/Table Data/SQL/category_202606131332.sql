b42b63952ad7a9e8ba374c3444eda699f7ffb732
Abirami Durairaj <adurai310@apac.comcast.com>
2026-06-13 17:00:42 +0530

    DB data dump 2026 june

---
+++ cashflow-portal/sql/CF DB - Prod backup/Table Data/SQL/category_202606131332.sql
@@ -0,0 +1,94 @@
+INSERT INTO PUBLIC.CATEGORY (CATEGORY_NAME,IS_ACTIVE,CREATED_AT,UPDATED_AT,SUB_CATEGORY,CATEGORY_ICON,SUBCATEGORY_ICON,NOTES) VALUES
+	 ('Investment',true,'2026-03-15 11:31:46.424492+00','2026-03-31 12:16:18.567861+00','RD','📈','🔐',NULL),
+	 ('Wifi',true,'2026-02-25 10:09:11.733718+00','2026-03-15 12:50:05.599004+00','Recharge','🌐','🔗',NULL),
+	 ('Home',true,'2026-02-25 10:09:11.733718+00','2026-05-21 05:44:16.59835+00','Provisions','🏠','🍚',NULL),
+	 ('Home',true,'2026-02-25 10:09:11.733718+00','2026-05-21 05:44:16.59835+00','Household Items','🏠','⚙️','Trays, Boxes, Mat, Mop Stick, Towels, Bed Sheet,'),
+	 ('Home',true,'2026-03-31 14:23:19.834614+00','2026-05-21 05:44:16.59835+00','Food','🏠','🍽️','Online - swiggy , zomato'),
+	 ('Home',true,'2026-02-25 10:09:11.733718+00','2026-05-21 05:44:16.59835+00','Groceries shop','🏠','🛒',NULL),
+	 ('Home',true,'2026-04-01 07:00:51.568674+00','2026-05-21 05:44:16.59835+00','Appa','🏠','💎','Tshirt, shoe, slipper, phone, jacket etc'),
+	 ('Home',true,'2026-04-01 10:54:07.327876+00','2026-05-21 05:44:16.59835+00','Dhanush','🏠','💎','Dress, kadan'),
+	 ('Home',true,'2026-04-02 10:56:52.481664+00','2026-05-21 05:44:16.59835+00','online - instamart','🏠','🛍️','provision , grocesary, house hold items'),
+	 ('Home',true,'2026-05-20 06:42:17.158355+00','2026-05-21 05:44:16.59835+00','sofa cupboard','🏠','🚛',NULL);
+INSERT INTO PUBLIC.CATEGORY (CATEGORY_NAME,IS_ACTIVE,CREATED_AT,UPDATED_AT,SUB_CATEGORY,CATEGORY_ICON,SUBCATEGORY_ICON,NOTES) VALUES
+	 ('Home',true,'2026-05-20 06:42:42.919677+00','2026-05-21 05:44:16.59835+00','AC','🏠','🔔',NULL),
+	 ('Home',true,'2026-05-21 05:44:16.36864+00','2026-05-21 05:44:16.834398+00','Ring','🏠','💍','25th anniversary'),
+	 ('BB',true,'2026-03-31 12:07:57.965913+00','2026-05-18 05:48:20.152992+00','Emi','🌍','⚡',NULL),
+	 ('BB',true,'2026-03-31 14:18:29.691362+00','2026-05-18 05:48:20.152992+00','transport','🌍','🚚','uber,dunzo package'),
+	 ('BB',true,'2026-04-01 09:28:19.218374+00','2026-05-18 05:48:20.152992+00','Phone recharge','🌍','📱',NULL),
+	 ('BB',true,'2026-03-31 12:08:27.375053+00','2026-05-18 05:48:20.152992+00','Gifts','🌍','🎁','Dress, shoes, skin care products'),
+	 ('BB',true,'2026-04-24 09:18:36.955286+00','2026-05-18 05:48:20.152992+00','Hault','🌍','📌','hault'),
+	 ('BB',true,'2026-05-17 11:29:21.679823+00','2026-05-18 05:48:20.152992+00','Astrologer','🌍','✏️',NULL),
+	 ('Abi',true,'2026-04-01 09:40:38.328417+00','2026-05-25 09:55:34.157066+00','Food Dominos','🧿','🍽️','online'),
+	 ('Food',false,'2026-02-25 10:09:11.733718+00','2026-03-31 14:22:26.636764+00','School','🍽️','👧',NULL);
+INSERT INTO PUBLIC.CATEGORY (CATEGORY_NAME,IS_ACTIVE,CREATED_AT,UPDATED_AT,SUB_CATEGORY,CATEGORY_ICON,SUBCATEGORY_ICON,NOTES) VALUES
+	 ('Food',false,'2026-02-25 10:09:11.733718+00','2026-03-31 14:22:29.808564+00','Colleges','🍽️','🎓',NULL),
+	 ('Food',false,'2026-02-25 10:09:11.733718+00','2026-03-31 14:22:32.297268+00','Office','🍽️','💻','ofc frnds team outing'),
+	 ('Food',false,'2026-03-31 13:41:10.416599+00','2026-03-31 14:22:36.694201+00','Home','🍽️','🏘️',NULL),
+	 ('Abi',true,'2026-03-31 14:41:44.679351+00','2026-05-25 09:55:34.157066+00','Accessory','🧿','💍','clip, hair band, earning, chain, braclet'),
+	 ('Abi',true,'2026-03-15 10:22:42.55+00','2026-05-25 09:55:34.157066+00','Transport','🧿','🚗','auto,cab'),
+	 ('Abi',true,'2026-03-15 11:56:26.034241+00','2026-05-25 09:55:34.157066+00','Medical','🧿','💊',NULL),
+	 ('Abi',true,'2026-02-25 10:09:11.733718+00','2026-05-25 09:55:34.157066+00','Phone Recharge','🧿','📱',NULL),
+	 ('Abi',true,'2026-02-25 10:09:11.733718+00','2026-05-25 09:55:34.157066+00','Dress','🧿','👗',NULL),
+	 ('Investment',true,'2026-03-31 12:15:36.163695+00','2026-03-31 12:15:36.641097+00','Stock','📈','💸',NULL),
+	 ('Abi',true,'2026-03-15 12:54:21.754885+00','2026-05-25 09:55:34.157066+00','Stationary','🧿','🎨',NULL);
+INSERT INTO PUBLIC.CATEGORY (CATEGORY_NAME,IS_ACTIVE,CREATED_AT,UPDATED_AT,SUB_CATEGORY,CATEGORY_ICON,SUBCATEGORY_ICON,NOTES) VALUES
+	 ('Investment',true,'2026-03-31 12:05:55.414805+00','2026-03-31 12:15:49.399361+00','PPF','📈','🔐',NULL),
+	 ('Abi',true,'2026-03-15 12:50:39.459449+00','2026-05-25 09:55:34.157066+00','Makeup','🧿','💄','Skin Care Products'),
+	 ('Investment',true,'2026-03-31 12:05:37.003646+00','2026-03-31 12:15:58.28194+00','MF','📈','🎯',NULL),
+	 ('Investment',true,'2026-03-31 12:01:39.387143+00','2026-03-31 12:16:07.732617+00','NPS','📈','🔐',NULL),
+	 ('Outing',false,'2026-02-25 10:09:11.733718+00','2026-04-01 09:37:23.206925+00','School Friends','🐾','👧',NULL),
+	 ('Abi',true,'2026-03-15 09:32:04.892509+00','2026-05-25 09:55:34.157066+00','Slippers','🧿','👠',NULL),
+	 ('Abi',true,'2026-03-15 13:14:58.807094+00','2026-05-25 09:55:34.157066+00','Hand bag','🧿','👜',NULL),
+	 ('Abi',true,'2026-03-15 13:15:49.369162+00','2026-05-25 09:55:34.157066+00','Water Bottle','🧿','🥤',NULL),
+	 ('Abi',true,'2026-03-31 14:35:26.674196+00','2026-05-25 09:55:34.157066+00','netflix','🧿','🖥️','subcription'),
+	 ('Abi',true,'2026-05-17 09:49:50.266513+00','2026-05-25 09:55:34.157066+00','BOB Edu loan Acc','🧿','💁‍♀️',NULL);
+INSERT INTO PUBLIC.CATEGORY (CATEGORY_NAME,IS_ACTIVE,CREATED_AT,UPDATED_AT,SUB_CATEGORY,CATEGORY_ICON,SUBCATEGORY_ICON,NOTES) VALUES
+	 ('Abi',true,'2026-05-18 05:56:28.916432+00','2026-05-25 09:55:34.157066+00','miscellaneous','🧿','₿',NULL),
+	 ('Abi',true,'2026-05-19 10:59:21.586876+00','2026-05-25 09:55:34.157066+00','shopping','🧿','🛒',NULL),
+	 ('Abi',true,'2026-05-21 06:42:22.799068+00','2026-05-25 09:55:34.157066+00','Trip - Chickmangalur','🧿','💳',NULL),
+	 ('Abi',true,'2026-02-25 10:09:11.733718+00','2026-05-25 09:55:34.157066+00','Online - Flipkart','🧿','🛍️',NULL),
+	 ('BB',true,'2026-04-01 10:41:20.41706+00','2026-05-18 05:48:20.152992+00','Kadan - debt','🌍','🏦','cash'),
+	 ('BB',true,'2026-05-17 11:29:41.369441+00','2026-05-18 05:48:20.152992+00','Others','🌍','🎉',NULL),
+	 ('Abi',true,'2026-03-15 12:11:06.41051+00','2026-05-25 09:55:34.157066+00','Online - Instamart','🧿','🛍️',NULL),
+	 ('Abi',true,'2026-02-25 10:09:11.733718+00','2026-05-25 09:55:34.157066+00','Online - Messho','🧿','🛍️',NULL),
+	 ('Abi',true,'2026-04-02 11:19:19.239603+00','2026-05-25 09:55:34.157066+00','office','🧿','💻','chris mom, team budget fr events & others'),
+	 ('Abi',true,'2026-04-02 10:47:29.74879+00','2026-05-25 09:55:34.157066+00','Repair','🧿','⚙️','watch, ect');
+INSERT INTO PUBLIC.CATEGORY (CATEGORY_NAME,IS_ACTIVE,CREATED_AT,UPDATED_AT,SUB_CATEGORY,CATEGORY_ICON,SUBCATEGORY_ICON,NOTES) VALUES
+	 ('Abi',true,'2026-04-02 09:37:33.198115+00','2026-05-25 09:55:34.157066+00','Relatives','🧿','💁‍♀️',NULL),
+	 ('Abi',true,'2026-04-01 07:05:18.226915+00','2026-05-25 09:55:34.157066+00','Temple','🧿','🔥',NULL),
+	 ('Abi',true,'2026-03-31 14:25:51.410112+00','2026-05-25 09:55:34.157066+00','Food School','🧿','🍽️','directly at shop'),
+	 ('Abi',true,'2026-03-31 14:25:19.594111+00','2026-05-25 09:55:34.157066+00','Food Office team lunch','🧿','🍽️','abi lunch break'),
+	 ('Abi',true,'2026-03-31 14:26:11.176372+00','2026-05-25 09:55:34.157066+00','Food College','🧿','🍽️',NULL),
+	 ('Abi',true,'2026-03-31 13:44:42.99465+00','2026-05-25 09:55:34.157066+00','Food Shop','🧿','🍽️','directly at shop'),
+	 ('Abi',true,'2026-03-31 13:44:16.421938+00','2026-05-25 09:55:34.157066+00','Food Zomato','🧿','🍽️','online'),
+	 ('Abi',true,'2026-03-31 13:43:29.179849+00','2026-05-25 09:55:34.157066+00','Food Swiggy','🧿','🍽️','online'),
+	 ('Abi',true,'2026-03-31 13:43:07.641198+00','2026-05-25 09:55:34.157066+00','Food Office lunch','🧿','🍽️','abi lunch break'),
+	 ('Abi',true,'2026-03-31 14:34:33.599097+00','2026-05-25 09:55:34.157066+00','google','🧿','🌐',NULL);
+INSERT INTO PUBLIC.CATEGORY (CATEGORY_NAME,IS_ACTIVE,CREATED_AT,UPDATED_AT,SUB_CATEGORY,CATEGORY_ICON,SUBCATEGORY_ICON,NOTES) VALUES
+	 ('Abi',true,'2026-03-31 14:40:14.823674+00','2026-05-25 09:55:34.157066+00','Film','🧿','📷',NULL),
+	 ('Abi',true,'2026-04-01 07:02:04.469327+00','2026-05-25 09:55:34.157066+00','scooty','🧿','🛵','fuel, service, insurance etc'),
+	 ('Abi',true,'2026-05-17 10:56:29.646415+00','2026-05-25 09:55:34.157066+00','gold loan','🧿','💷','sbi'),
+	 ('Home',true,'2026-03-31 14:40:37.30329+00','2026-05-21 05:44:16.59835+00','Amma','🏠','💎',NULL),
+	 ('Home',true,'2026-03-31 14:45:34.845822+00','2026-05-21 05:44:16.59835+00','Groceries Instamart','🏠','🛒',NULL),
+	 ('Home',true,'2026-04-01 06:59:04.067238+00','2026-05-21 05:44:16.59835+00','Rajeswari','🏠','🕶️','Bed matrix'),
+	 ('Home',true,'2026-04-02 11:52:44.109246+00','2026-05-21 05:44:16.59835+00','online - blinkit','🏠','🛍️','provision , grocesary, house hold items'),
+	 ('Home',true,'2026-05-20 05:50:07.170606+00','2026-05-21 05:44:16.59835+00','Trip - Uttrakand','🏠','💳',NULL),
+	 ('Home',true,'2026-05-20 06:44:06.528961+00','2026-05-21 05:44:16.59835+00','dress plastic cupboard','🏠','🎀',NULL),
+	 ('Home',true,'2026-05-16 16:18:37.571378+00','2026-05-21 05:44:16.59835+00','recharge','🏠','📱',NULL);
+INSERT INTO PUBLIC.CATEGORY (CATEGORY_NAME,IS_ACTIVE,CREATED_AT,UPDATED_AT,SUB_CATEGORY,CATEGORY_ICON,SUBCATEGORY_ICON,NOTES) VALUES
+	 ('Home',true,'2026-05-16 16:48:33.445615+00','2026-05-21 05:44:16.59835+00','chit','🏠','💲','premela'),
+	 ('Home',true,'2026-05-20 07:06:59.636843+00','2026-05-21 05:44:16.59835+00','decors','🏠','🎉','wall frames, miselaneous'),
+	 ('BB',true,'2026-05-18 05:48:19.885718+00','2026-05-18 05:56:29.463005+00','miscellaneous','🌍','₿','basic needs'),
+	 ('Abi',true,'2026-03-13 15:39:16.574439+00','2026-05-25 09:55:34.157066+00','Online - Amazon','🧿','🛍️',NULL),
+	 ('Abi',true,'2026-04-02 10:46:08.445689+00','2026-05-25 09:55:34.157066+00','Online - Myntra','🧿','🛍️',NULL),
+	 ('Abi',true,'2026-04-02 10:50:30.336833+00','2026-05-25 09:55:34.157066+00','hair','🧿','👧',NULL),
+	 ('Abi',true,'2026-04-02 11:02:08.810142+00','2026-05-25 09:55:34.157066+00','jio hotstar','🧿','🖥️','subcription'),
+	 ('Abi',true,'2026-04-01 07:10:55.123528+00','2026-05-25 09:55:34.157066+00','snacks','🧿','🍫',NULL),
+	 ('Abi',true,'2026-04-02 11:12:46.898326+00','2026-05-25 09:55:34.157066+00','college','🧿','🎓','friends'),
+	 ('Abi',true,'2026-04-24 09:52:19.424429+00','2026-05-25 09:55:34.157066+00','Friends Gift','🧿','🎁',NULL);
+INSERT INTO PUBLIC.CATEGORY (CATEGORY_NAME,IS_ACTIVE,CREATED_AT,UPDATED_AT,SUB_CATEGORY,CATEGORY_ICON,SUBCATEGORY_ICON,NOTES) VALUES
+	 ('Abi',true,'2026-04-24 10:02:31.714352+00','2026-05-25 09:55:34.157066+00','God','🧿','🔥',NULL),
+	 ('Abi',true,'2026-05-16 16:44:53.248067+00','2026-05-25 09:55:34.157066+00','crypto lost','🧿','🪙',NULL),
+	 ('Abi',true,'2026-05-17 09:38:20.717097+00','2026-05-25 09:55:34.157066+00','contact lenz','🧿','🕶️',NULL),
+	 ('Abi',true,'2026-05-20 05:50:22.982873+00','2026-05-25 09:55:34.157066+00','Trip - Kerala','🧿','💳',NULL),
+	 ('Abi',true,'2026-05-25 09:55:33.892508+00','2026-05-25 09:55:34.427977+00','pratheek kadan','🧿','💳',NULL) ON CONFLICT ;
